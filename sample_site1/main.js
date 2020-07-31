33b9be100be003530216778176bbcca83cb88575
Blue89Azul <life.89.blue@gmail.com>
2020-07-31 09:37:55 +0900

    スマホ画面サイドバーフェードイン

--- sample_site1/main.js
+++ sample_site1/main.js
@@ -1,7 +1,8 @@
 $(function() {
   // スマホメニュー開閉
-  $(".sp-menu").on("click", addClass);
+  $(".sp-menu").on("mouseover", addClass);
   $(".sp-menu-items").on("click", removeClass);
+  $(window).scroll(menuStyle);
 
   function addClass() {
     $(".sp-menu-items").addClass("sp-menu-items-open");
@@ -9,6 +10,14 @@ $(function() {
 
   function removeClass() {
     $(".sp-menu-items").removeClass("sp-menu-items-open");
+  }
+
+  function menuStyle(){
+    if($(this).scrollTop() > 100){
+      $(".sp-menu").addClass("sp-menu-style");
+    } else {
+      $(".sp-menu").removeClass("sp-menu-style");
+    }
   }
 
 });
